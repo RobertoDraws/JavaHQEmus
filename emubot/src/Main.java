721e623263a41695a36e45b9c39e67938686ba99
Jake Crowley <jakecrow@gmail.com>
2018-07-15 13:06:45 -0400

    Added partial cashout functionality. (Unfinished)

--- emubot/src/Main.java
+++ emubot/src/Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static int accountsLimit = 30;
+    public static int accountsLimit = 2880;
     public static MainGUI gui;
     public static ArrayList<HQ_API> HQAccounts = new ArrayList<>();
 
@@ -17,7 +17,7 @@ public class Main {
 
         try{
             //should add separate files later
-            Scanner s = new Scanner(new File("Jake.txt"));
+            Scanner s = new Scanner(new File("tokens.txt"));
             while(s.hasNextLine() && HQAccounts.size() < accountsLimit){
                 HQAccounts.add(new HQ_API(s.nextLine()));
             }
